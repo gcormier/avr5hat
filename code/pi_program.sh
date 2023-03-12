086071abfe567bc54f3ea9dc36a71535c2f4942d
Greg Cormier <gcormier@gmail.com>
2023-03-12 19:06:16 -0400

    avrdude.conf

--- code/pi_program.sh
+++ code/pi_program.sh
@@ -17,5 +17,5 @@ sudo avrdude -p $DEVICE -c linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -b 125000
 sleep 0.1
 
 #program flash and lock bits
-#sudo avrdude -p $DEVICE -c linuxspi -P /dev/spidev0.0 -b 2000000 -D -v -u -U flash:w:$firmware:i 2>/home/pi/flash_results.txt
+#sudo avrdude -p $DEVICE -c linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -b 2000000 -D -v -u -U flash:w:$firmware:i 2>/home/pi/flash_results.txt
 
