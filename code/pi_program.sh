dd0234b63fb48b856a3569a9b586b2b4bc6d9e6a
Greg Cormier <gcormier@gmail.com>
2023-03-12 19:24:57 -0400

    updat4e

--- code/pi_program.sh
+++ code/pi_program.sh
@@ -10,12 +10,9 @@ LOW_FUSE=0xE2
 HIGH_FUSE=0xD6
 EXT_FUSE=0xFE
 
-#erase and then write FUSE bits
-#-C /home/pi/avrdude_gpio.conf
-sudo avrdude -p $DEVICE -c linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -b 125000 -D -v -U hfuse:w:$HIGH_FUSE:m -u -U lfuse:w:$LOW_FUSE:m -u -U efuse:w:$EXT_FUSE:m
+# Write fuses
+sudo avrdude -p $DEVICE -C+./avrdude.avr5hat.conf -c avrhat_linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -B 125khz -D -v -U hfuse:w:$HIGH_FUSE:m -U lfuse:w:$LOW_FUSE:m -U efuse:w:$EXT_FUSE:m
 
-sleep 0.1
-
-#program flash and lock bits
-#sudo avrdude -p $DEVICE -c linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -b 2000000 -D -v -u -U flash:w:$firmware:i 2>/home/pi/flash_results.txt
+# Write program
+sudo avrdude -p $DEVICE -C+./avrdude.avr5hat.conf -c avrhat_linuxspi -P /dev/spidev0.0:/dev/gpiochip0:0 -B 2mhz -D -v -U flash:w:$firmware:i
 
